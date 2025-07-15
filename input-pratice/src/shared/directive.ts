664f40943bb07caecdcc4750d372bd4d19a1b5b3
Tô Hải Đăng <tohaidang161@gmail.com>
2025-07-15 10:41:01 +0700

    fix custom validator

--- input-pratice/src/shared/directive.ts
+++ input-pratice/src/shared/directive.ts
@@ -1,6 +1,5 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-/** A hero's name can't match the given regular expression */
 export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const forbidden = nameRe.test(control.value);
