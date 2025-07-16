7cac9567658aa55e29eed4215ce1558b613cae77
Tô Hải Đăng <tohaidang161@gmail.com>
2025-07-16 11:18:23 +0700

    config router

--- task-1/src/app/app-routing.module.ts
+++ task-1/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { UserListComponent } from './feature/user/pages/user-list/user-list.component';
+import { UserDetailComponent } from './feature/user/pages/user-detail/user-detail.component';
+import { UserUpdateComponent } from './feature/user/pages/user-update/user-update.component';
+import { NotFoundPageComponent } from './shared/components/not-found-page/not-found-page.component';
+import { UserCreateComponent } from './feature/user/pages/user-create/user-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: UserListComponent },
+      { path: 'detail/:userId', component: UserDetailComponent },
+      { path: 'update/:userId', component: UserUpdateComponent },
+      { path: 'create', component: UserCreateComponent },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- task-1/src/app/app.module.ts
+++ task-1/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { TextInputComponent } from './shared/components/text-input/text-input.component';
+import { UserListComponent } from './feature/user/pages/user-list/user-list.component';
+import { UserDetailComponent } from './feature/user/pages/user-detail/user-detail.component';
+import { UserUpdateComponent } from './feature/user/pages/user-update/user-update.component';
+import { UserCreateComponent } from './feature/user/pages/user-create/user-create.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { NotFoundPageComponent } from './shared/components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TextInputComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserUpdateComponent,
+    UserCreateComponent,
+    MainLayoutComponent,
+    NotFoundPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
